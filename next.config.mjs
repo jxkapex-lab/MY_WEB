06584eda4a8a41f705db46ef4889103695011792
Krittin Tunruangchat <tukrittin@powerbuy.co.th>
2026-09-21 17:26:08 +0700

    fix: ensure assets and .nojekyll load correctly on GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: isGitHubActions ? '/MY_WEB' : '',
+  assetPrefix: isGitHubActions ? '/MY_WEB/' : undefined,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
